83da29a6ccab5ea2a87d99f22b526ec9623bcae9
Julianna Tetreault <juliannatetreault@gmail.com>
2020-12-15 17:47:25 -0700

    Log moves

--- app/move.rb
+++ app/move.rb
@@ -2,11 +2,11 @@
 # Valid moves are "up", "down", "left", or "right".
 # TODO: Use the information in board to decide your next move.
 def move(board)
-  puts board
+  logger.info board
 
   # Choose a random direction to move in
   possible_moves = ["up", "down", "left", "right"]
   move = possible_moves.sample
-  puts "MOVE: " + move
+  logger.info "MOVE: " + move
   { "move": move }
 end
